a6f3ed3d969a9546e54b14bebc7cce9c00bc7376
Zukky55 <www.kase@gmail.com>
2018-11-10 13:55:35 +0900

    first commit

---
+++ Singleton/RegisterNote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class RegisterNote
+    {
+        private static RegisterNote registerNote = new RegisterNote();
+        private RegisterNote() { }
+
+        public static RegisterNote GetInstance()
+        {
+            return registerNote;
+        }
+    }
+}
